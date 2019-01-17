51a38405d9e1d1a8ead1c5e0f41a54b628ae44ba
f1nesse13 <philly.teg@gmail.com>
2019-01-16 22:14:47 -0500

    stack class implemented

---
+++ W1D1/DIY_ADTs/stack_ex1.rb
@@ -0,0 +1,35 @@
+class Stack
+
+  attr_reader :stack
+
+  def initialize
+    @stack = Array.new
+  end
+
+  def push(el)
+    @stack.push(el)
+  end
+
+  def pop
+    @stack.pop
+  end
+
+  def peek
+    @stack.last
+  end
+end
+
+a = Stack.new
+
+a.push("ele1")
+a.push("ele2")
+a.push("ele3")
+a.push("ele4")
+p a.push("ele5")
+ a.pop
+p a.push("ele6")
+ a.pop
+p a.peek
+ a.pop
+p a.peek
+p a.stack
