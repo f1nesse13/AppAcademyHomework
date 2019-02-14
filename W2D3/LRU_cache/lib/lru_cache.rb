f20d50a287d56c283ca9f92fbbb04fac36983c62
f1nesse13 <philly.teg@gmail.com>
2019-02-13 21:23:18 -0500

    file structure for W2D3

--- W2D3/LRU_cache/lib/lru_cache.rb
+++ W2D3/LRU_cache/lib/lru_cache.rb
@@ -1,20 +1,29 @@
 class LRUCache
-  def initialize
+  def initialize(max_size)
+    @cache = []
+    @max_size = max_size
   end
 
   def count
     # returns number of elements currently in cache
+    @cache_size.count
   end
 
   def add(el)
     # adds element to cache according to LRU principle
+    @cache.check_length
+    @cache.push(el)
   end
 
   def show
     # shows the items in the cache, with the LRU item first
+    print @cache
   end
 
   private
   # helper methods go here!
 
+  def check_length
+    @cache.shift if @cache.count >= @max_size
+  end
 end
