ef5370f618eafcf1e022aab86e965952bea731f5
f1nesse13 <philly.teg@gmail.com>
2019-02-13 21:53:49 -0500

    added specs and helpers for lru_cache

--- W2D3/LRU_cache/lib/lru_cache.rb
+++ W2D3/LRU_cache/lib/lru_cache.rb
@@ -1,4 +1,6 @@
 class LRUCache
+
+  attr_reader :cache
   def initialize(max_size)
     @cache = []
     @max_size = max_size
@@ -6,13 +8,17 @@ class LRUCache
 
   def count
     # returns number of elements currently in cache
-    @cache_size.count
+    @cache.count
   end
 
   def add(el)
     # adds element to cache according to LRU principle
-    @cache.check_length
-    @cache.push(el)
+    self.check_length
+    if element_exists?(el)
+      most_recently_used(el)
+    else
+      @cache.push(el)
+    end
   end
 
   def show
@@ -20,10 +26,37 @@ class LRUCache
     print @cache
   end
 
-  private
-  # helper methods go here!
-
   def check_length
     @cache.shift if @cache.count >= @max_size
   end
+  
+  private
+
+  def element_exists?(ele)
+    @cache.include?(ele)
+  end
+
+  def most_recently_used(ele)
+    element = @cache.delete(ele)
+    @cache.push(element)
+  end
 end
+# if $PROGRAM_NAME == __FILE__
+#   johnny_cache = LRUCache.new(4)
+
+#   johnny_cache.add("I walk the line")
+#   johnny_cache.add(5)
+
+#   johnny_cache.count # => returns 2
+
+#   johnny_cache.add([1,2,3])
+#   johnny_cache.add(5)
+#   johnny_cache.add(-5)
+#   johnny_cache.add({a: 1, b: 2, c: 3})
+#   johnny_cache.add([1,2,3,4])
+#   johnny_cache.add("I walk the line")
+#   johnny_cache.add(:ring_of_fire)
+#   johnny_cache.add("I walk the line")
+#   johnny_cache.add({a: 1, b: 2, c: 3})
+#   johnny_cache.show
+# end
---
+++ W2D3/LRU_cache/spec/lru_cache_spec.rb
@@ -0,0 +1,41 @@
+require 'rspec'
+require 'lru_cache'
+
+describe LRUCache do
+  subject(:cache) { LRUCache.new(3) }
+  
+  describe '#initialize' do
+    it 'initializes with a empty array' do
+      expect(cache.cache).to eq([])
+    end
+  end
+
+  describe '#add' do
+    before(:each) { cache.add(5) }
+    it 'adds the correct element' do
+      expect(cache.cache).to eq([5])
+    end
+
+    it 'adds more than one element' do
+      cache.add(6)
+      expect(cache.cache).to eq([5, 6])
+    end
+
+    it 'will keep array size under cache size' do
+      cache.add(1)
+      cache.add(2)
+      cache.add(3)
+      cache.add(4)
+      expect(cache.cache).to eq([2, 3, 4])
+    end
+  end
+
+  describe '#count' do
+    it 'returns the correct number of elements' do
+      cache.add(1)
+      expect(cache.count).to_not eq(0)
+    end
+  end
+  
+
+end
