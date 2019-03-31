720a627810841d508fc24d2df370803a6a698ef7
Joe Pell <jpell3130@gmail.com>
2019-03-31 16:17:08 -0400

    added query to route model

--- W2D6/N+1_buster/n+1_buster/app/models/route.rb
+++ W2D6/N+1_buster/n+1_buster/app/models/route.rb
@@ -1,6 +1,6 @@
 class Route < ApplicationRecord
   has_many :buses,
-    class_name: 'Bus',
+    class_name: "Bus",
     foreign_key: :route_id,
     primary_key: :id
 
@@ -20,6 +20,15 @@ class Route < ApplicationRecord
   end
 
   def better_drivers_query
-    # TODO: your code here
+    buses = self.buses.includes(:drivers)
+    all_drivers = {}
+    buses.each do |bus|
+      drivers = []
+      bus.drivers.each do |driver|
+        drivers << driver.name
+      end
+      all_drivers[bus.id] = drivers
+    end
+    all_drivers
   end
 end
