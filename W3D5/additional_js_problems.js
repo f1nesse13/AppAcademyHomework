393940d80be9f363f4372df467763a4591ed2880
Joe Pell <jpell3130@gmail.com>
2019-06-05 00:07:59 -0400

    added Elephant class with instance methods

--- W3D5/additional_js_problems.js
+++ W3D5/additional_js_problems.js
@@ -15,4 +15,32 @@ const printNames = (arr) => {
   });
 }
 
-titleize(["John", "Joe", "Jack"], printNames);
+// titleize(["John", "Joe", "Jack"], printNames);
+
+
+// Phase 2
+
+function Elephant(name, height, tricks) {
+  this.name = name;
+  this.height = height;
+  this.tricks = tricks;
+};
+// const elephant1 = new Elephant("Joey", 54, ["riding a bike"]);
+
+Elephant.prototype.trumpet = function() {
+  console.log(`${this.name} the elephant goes 'phrRRRRRRRRRRR!!!!!!!'`)
+};
+
+Elephant.prototype.grow = function () {
+  this.height += 12;
+};
+
+Elephant.prototype.addTrick = function (trick) {
+  this.tricks.push(trick);
+};
+
+Elephant.prototype.play = function () {
+  index = Math.floor(Math.random() * this.tricks.length);
+  console.log(this.tricks[index])
+}
+
