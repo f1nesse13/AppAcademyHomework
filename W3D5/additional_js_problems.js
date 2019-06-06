aaddca6032901e1166a98014e84e77b75a7b0eb9
Joe Pell <jpell3130@gmail.com>
2019-06-05 21:00:10 -0400

    paradeHelper and dinerBreakfast added (last 2 phases)

--- W3D5/additional_js_problems.js
+++ W3D5/additional_js_problems.js
@@ -44,3 +44,32 @@ Elephant.prototype.play = function () {
   console.log(this.tricks[index])
 }
 
+// phase 3
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+let herd = [ellie, charlie, kate, micah];
+
+Elephant.paradeHelper = function (elephant) {
+  console.log(`${elephant.name} is trotting by!`)
+}
+
+Elephant.paradeHelper(micah)
+
+herd.forEach(Elephant.paradeHelper);
+
+// phase 4
+function dinerBreakfast() {
+  let order = ["cheesy scrambled eggs"]
+  console.log(`I'd like ${order} please.`)
+    return function (food) {
+      order.push(food)
+      orders = order.join(" and ")
+      console.log(`I'd like ${orders} please.`)
+    }
+}
+
+
+let breakFast = dinerBreakfast();
