1294d5f4554a1fa0a5d011cb4980dff024ee92a1
Joe Pell <jpell3130@gmail.com>
2019-06-26 14:17:17 -0400

    phase2 implemented - adding SF places to list

--- W4D4/vanilla_DOM/script.js
+++ W4D4/vanilla_DOM/script.js
@@ -1,31 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   // toggling restaurants
 
-  const toggleLi = (e) => {
+  const toggleLi = e => {
     const li = e.target;
-    if (li.className === "visited") {
-      li.className = "";
+    if (li.className === 'visited') {
+      li.className = '';
     } else {
-      li.className = "visited";
+      li.className = 'visited';
     }
   };
 
-  document.querySelectorAll("#restaurants li").forEach((li) => {
-    li.addEventListener("click", toggleLi);
+  document.querySelectorAll('#restaurants li').forEach(li => {
+    li.addEventListener('click', toggleLi);
   });
-
-
 
   // adding SF places as list items
 
   // --- your code here!
-
-
+  document.querySelector('.favorite-submit').addEventListener('click', function(event) {
+    event.preventDefault();
+    const ul = document.querySelector('#sf-places');
+    const input = document.querySelector('.favorite-input');
+    let li = document.createElement('li');
+    li.textContent = input.value;
+    ul.appendChild(li);
+    input.value = '';
+  });
 
   // adding new photos
 
   // --- your code here!
-
-
-
 });
